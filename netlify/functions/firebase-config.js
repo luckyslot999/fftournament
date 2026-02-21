89c7338d366c6334852cda98d390bbb908a5a7f9
EWA GAMES <modspak4@gmail.com>
2026-02-22 03:14:22 +0500

    Add files via upload

---
+++ netlify/functions/firebase-config.js
@@ -0,0 +1,19 @@
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.FIREBASE_DATABASE_URL,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.FIREBASE_APP_ID,
+            measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        })
+    };
+};
